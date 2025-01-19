a5fd10147ab3fd33e005bbac727b7d285fd9444b
MaxineToTheStars <146043018+MaxineToTheStars@users.noreply.github.com>
2025-01-18 19:40:03 -0500

    Project status upload

---
+++ include/ignition.h
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2025 Astrl.
+ *
+ * This file is part of Ignition. It is subject to the license terms in
+ * the LICENSE file found in the top-level directory of this project and at
+ * https://github.com/The-Astrl-Project/ignition/blob/HEAD/LICENSE.
+ *
+ * This file may not be copied, modified, propagated, or distributed
+ * except according to the terms contained in the LICENSE file.
+ */
+
+// Header Declarations
+// ----------------------------------------------------------------
+
+// ---
+
+// ---
+
+// ----------------------------------------------------------------
+
+// Macro Definitions
+
+// File Docstring
+// --------------------------------
+// Ignition || src/ignition.c <-> include/ignition.h
+//
+// Handles both Ignition: Boot and Ignition: Init systems. This
+// is only the parent codebase, the implementation logic can be found
+// under src/modules
+//
+// @author @MaxineToTheStars <https://github.com/MaxineToTheStars>
+// ----------------------------------------------------------------
+
+// Struct Definitions
+
+// Enum Definitions
+
+// Type Definitions
+
+// Variable Definitions
+
+// Main
+
+// Methods
